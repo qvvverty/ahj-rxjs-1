283370ed8bd488b18208ae7928f08c6fd43ba77a
Leonid Kostyuchenko <kstchnko@gmail.com>
2021-07-31 18:02:19 +0300

    improve layout

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,22 @@ import { concatMap } from 'rxjs/operators';
 const unread$ = interval(5000)
   .pipe(concatMap(() => ajax.getJSON('http://localhost:7070/messages/unread')));
 unread$.subscribe((value) => console.log(value));
+
+unread$.subscribe((value) => {
+  const msgContainer = document.querySelector('div.incoming');
+  msgContainer.insertAdjacentHTML('afterBegin', `
+    <div class="message">
+      <div class="name-wrapper">
+        <div class="email">
+          ${value.from}
+        </div>
+        <div class="subject">
+          ${value.subject}
+        </div>
+      </div>
+      <div class="date-wrapper">
+        ${value.received}
+      </div>
+    </div>
+  `);
+});
